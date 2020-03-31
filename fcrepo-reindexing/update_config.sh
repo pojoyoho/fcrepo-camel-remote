fda535cde77b9f3af1f7799348ec167ab255603c
pojoyoho <pojoyoho@gmail.com>
2020-03-31 11:08:29 -0600

    Update messages

--- fcrepo-reindexing/update_config.sh
+++ fcrepo-reindexing/update_config.sh
@@ -2,7 +2,7 @@
 input="object_paths.txt"
 while IFS= read -r line
 do
-  echo "Updating $line ..."
+  echo "$(date): Updating $line ..."
   # update object path with escape
   escpath=$(echo $line | sed 's/\//\\\//g')
   # update identifier in config.json
@@ -11,7 +11,7 @@ do
   git commit -a -m "Update identifier to $line"
   git push
   # sleep for random 10 to 60 minutes
-  min=$(jot -r 20 60)
-  echo "$(date) Waiting $min minutes..."
+  min=$(jot -r 10 60)
+  echo "$(date): Waiting $min minutes..."
   sleep $((min*60))
 done < "$input"
