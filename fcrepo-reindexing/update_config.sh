37b7ace62b30932399ade5eb66b20c2ab54503dd
pojoyoho <pojoyoho@gmail.com>
2020-04-19 16:06:51 -0600

    Update identifier to /prod/fb/77/54/34/fb775434-b9a5-4d2b-85cb-25c0f6d6fb0b/members

--- fcrepo-reindexing/update_config.sh
+++ fcrepo-reindexing/update_config.sh
@@ -2,16 +2,19 @@
 input="object_paths.txt"
 while IFS= read -r line
 do
+  # remove trailing carrier return
+  line=$( echo $line | tr -d '\r')
   echo "$(date): Updating $line ..."
   # update object path with escape
   escpath=$(echo $line | sed 's/\//\\\//g')
   # update identifier in config.json
-  sed -i '' 's/\/prod[^"]*/'"$escpath"'/' config.json
+  sed 's/\/prod[^"]*/'"$escpath"'/' config.json
+  
   # commit and push
   git commit -a -m "Update identifier to $line"
   git push
-  # sleep for random 10 to 60 minutes
-  min=$(jot -r 1 10 60)
+  # sleep for random N to NN minutes
+  min=$(jot -r 1 3 10)
   echo "$(date): Waiting $min minutes..."
   sleep $((min*60))
 done < "$input"
