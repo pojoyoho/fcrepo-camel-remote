4f4e53f4b2526b7c7840af791e8619213d90d5b1
pojoyoho <pojoyoho@gmail.com>
2020-08-24 11:06:23 -0600

    Update identifier to /prod/b1/ab/1c/d0/b1ab1cd0-aef2-4fb8-bd34-7550fcbc1587/member_of_collections

--- fcrepo-reindexing/update_config.sh
+++ fcrepo-reindexing/update_config.sh
@@ -6,7 +6,7 @@ do
   if [ $((i++)) -gt 0 ] 
   then
     # sleep for random N to NN minutes
-    min=$(jot -r 1 2 5)
+    min=$(jot -r 1 3 8)
     echo "$(date): Waiting $min minutes..."
     sleep $((min*60))
   fi
